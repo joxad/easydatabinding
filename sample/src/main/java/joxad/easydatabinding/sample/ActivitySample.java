a5a4cf36c92f87285448900d802259673db8bba6
Josh <jdavid@phoceis.com>
2016-05-24 15:26:40 +0200

    Add test actiivty

---
+++ sample/src/main/java/joxad/easydatabinding/sample/ActivitySample.java
@@ -0,0 +1,16 @@
+package joxad.easydatabinding.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by josh on 24/05/16.
+ */
+public class ActivitySample extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
