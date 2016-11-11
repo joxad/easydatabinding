3bec868cf43933365d25df65417d5fb6a7edca71
Jocelyn David <external.z17jdavi>
2016-11-11 13:10:24 +0100

    Add support v4 and normal fragment

--- lib/src/main/java/com/joxad/easydatabinding/fragment/v4/FragmentBase.java
+++ lib/src/main/java/com/joxad/easydatabinding/fragment/v4/FragmentBase.java
@@ -1,4 +1,4 @@
-package com.joxad.easydatabinding.fragment;
+package com.joxad.easydatabinding.fragment.v4;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
---
+++ lib/src/main/java/com/joxad/easydatabinding/fragment/v4/FragmentBaseVM.java
@@ -0,0 +1,58 @@
+package com.joxad.easydatabinding.fragment.v4;
+
+import android.databinding.BaseObservable;
+import android.databinding.ViewDataBinding;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.joxad.easydatabinding.base.IVM;
+
+
+/**
+ * Created by josh on 13/04/16.
+ */
+public abstract class FragmentBaseVM<F extends FragmentBase, B extends ViewDataBinding> extends BaseObservable implements IVM {
+
+    protected final Handler handler;
+    protected final Handler uiHandler;
+    /***
+     * {@link F} is the fragment that use the current VM
+     */
+    protected F fragment;
+    /**
+     * {@link B} will be used to find the views inside the fragment
+     */
+    protected B binding;
+
+    /***
+     * @param
+     * @param binding
+     */
+    public FragmentBaseVM(F fragment, B binding) {
+        this.fragment = fragment;
+        this.binding = binding;
+        this.handler = new Handler();
+        this.uiHandler = new Handler(Looper.getMainLooper());
+        onCreate();
+    }
+
+    public abstract void onCreate();
+
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+
+    /**
+     * This is optionnal => use it if needed
+     */
+    @Override
+    public void onDestroy() {
+
+    }
+}
